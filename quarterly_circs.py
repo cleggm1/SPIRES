ed8ebe6e5a1d4e8dac22e1abc53c3782e0a92f0d
Melissa Clegg <cleggm1@users.noreply.github.com>
2017-01-26 11:53:47 -0600

    Create quarterly_circs.py

---
+++ quarterly_circs.py
@@ -0,0 +1,21 @@
+import re
+
+uses = []
+With open('usage_stats.in', 'r') as input:
+    for line in input.readlines():
+        """Jan-Mar"""
+#        matchObj = re.search(r'DATE-BORROWED = 0[123]/\d{2}/17;', line)
+        """Apr-June"""
+#        matchObj = re.search(r'DATE-BORROWED = 0[456]/\d{2}/17;', line)
+        """Jul-Sep"""
+#        matchObj = re.search(r'DATE-BORROWED = 0[789]/\d{2}/17;', line)
+        """Oct-Dec"""
+#        matchObj = re.search(r'DATE-BORROWED = 1[012]/\d{2}/17;', line)
+        if matchObj:
+#            print line
+            uses.append(line)
+
+print "%i circs in quarter" % len(uses)
+with open('usage_stats.in', 'r') as output:
+    output.writelines(uses)
+
